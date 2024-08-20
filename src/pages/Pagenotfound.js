d18cc446e0d14663b59c9f9a44bd038360e91789
divya2303 <divyaguptadg365@gmail.com>
2024-08-20 13:14:36 +0530

    first commit

--- src/pages/Pagenotfound.js
+++ src/pages/Pagenotfound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
+import Home from "./Home";
 
 const Pagenotfound = () => {
   return (
     <Layout>
-      <h1>Page Not Found</h1>
+      <Home/>
     </Layout>
   );
 };
